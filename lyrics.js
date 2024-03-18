3fc40f88f437fbd0aa68c481656ca2e67b5eaa95
danderbas <146648856+danderbas@users.noreply.github.com>
2024-03-18 10:30:32 -0300

    Add files via upload

---
+++ lyrics.js
@@ -0,0 +1,288 @@
+const letras = [
+	{
+		titulo: [
+			"O fortuna",
+			"Oh fortuna"
+		],
+		//comentarios: "Asi era.",
+		letra:
+		[
+			[
+`O Fortuna,
+velut luna
+statu variabilis,`
+				,
+`Oh Fortuna,
+cambias
+como la luna`
+			]
+			,
+			[
+`semper crescis
+aut decrescis;
+vita detestabilis`
+				,
+`siempre creces
+y decreces;
+vida detestable`
+			]
+			,
+			[
+`nunc obdurat
+et tunc curat
+ludo mentis aciem`
+				,
+`primero oprimes
+y luego alivias,
+juegas con la mente`
+			]
+			,
+			[
+`egestatem,
+potestatem,
+dissolvit ut glaciem.`
+				,
+`pobreza
+poder,
+los disuelves como al hielo` 
+			]
+			,
+			[
+`Sors immanis
+et inanis,
+rota tu volubilis,`
+				,
+`Destino monstruoso
+y vacio,
+tu rueda gira,`
+			]
+			,
+			[
+`status malus,
+vana salus
+semper dissolubilis,`
+				,
+`eres malevola,
+vana salvacion
+siempre desintegrandose`
+			]
+			,
+			[
+`obumbrata
+et velata
+michi quoque niteris`
+				,
+`ensombrecido
+y cubierto,
+brillas tambien para mi`
+			]
+			,
+			[
+`nunc per ludum
+dorsum nudum
+fero tui sceleris.`
+				,
+`ahora en el juego,
+con el dorso desnudo
+recibo tu villania`
+			]
+			,
+			[
+`Sors salutis
+et virtutis
+michi nunc contraria;`
+				,
+`La suerte se pone en mi contra,
+en salud
+y virtud`
+			]
+			,
+			[
+`est affectus
+et defectus
+semper in angaria.`
+				,
+`hay sentimiento
+y debilidad,
+siempre encadenados`
+			]
+			,
+			[
+`Hac in hora
+sine mora
+corde pulsum tangite;`
+				,
+`En esta hora
+sin demora
+pulsas la cuerda`
+			]
+			,
+			[
+`quod per sortem
+sternit fortem,
+mecum omnes plangite!`
+				,
+`la cual por destino
+golpea a los fuertes,
+todos lloren conmigo!`
+			]
+		]
+	}
+	,
+	{
+		titulo: ["Fortune plango vulnera", "Lamento las heridas de la fortuna"],
+		letra: [
+		[
+`Fortune plango vulnera,
+stillantibus ocellis
+quod sua michi munera
+subtrahit rebellis.`
+,
+`Lamento las heridas de la fortuna,
+con lagrimas en los ojos
+lo que una vez me obsequio
+lo retira perversamente`
+]
+,
+[
+`Verum est, quod legitur
+fronte capillata
+sed plerumque sequitur
+Occasio calvata.`
+,
+`Es verdad, esta escrito
+de frente tiene una fina cabellera
+pero cuando se persigue la oportunidad
+ella es calva`
+]
+,
+[
+`In Fortune solio
+sederam elatus,
+prosperitatis vario
+flore coronatus;`
+,
+`En el trono de la fortuna
+me sente en lo alto
+coronado por
+flores de prosperidad`
+]
+,
+[
+`quicquid enim florui
+felix et beatus,
+nunc a summo corrui
+gloria privatus.`
+,
+`Pese a haber florecido
+feliz y bendecido,
+ahora caigo de la cima
+privado de gloria`
+]
+,
+[
+`fortune rota volvitur,
+descendo minoratus;
+alter in altum tollitur
+nimis exaltatus`
+,
+`Gira la rueda de la fortuna
+voy abajo, disminuido;
+otro es ascendido
+a lo mas alto`
+]
+,
+[
+`rex sedet in vertice
+caveat ruinam!
+nam sub axe legimus:
+hecubam reginam`
+,
+`al rey sentado en la punta
+dejadle temer la ruina!
+porque debajo del eje se lee:
+reina Hecuba`
+]
+]
+
+	}
+	,
+	{
+		titulo: ["Veris leta facies", "La cara jovial de la primavera"],
+		letra: [
+			[
+`Veris leta facies
+mundo propinatur,`
+			,
+			`La cara jovial de la primavera,
+			gira en torno al mundo`
+			]
+			,
+			[
+`hiemalis acies
+victo iam fugatur;`
+				,
+				`el invierno crudo,
+				vencido hace su retirada`]
+			,
+			[
+`in vestitu vario
+Flora principatur,
+nemorum dulcisono
+que cantu celebrantur.`
+				,
+				`vestida en varios ornamentos,
+				Flora reina,
+				el dulce sonido de los bosques
+				la alaban cantando`
+			]
+			,
+			[`Flore fusus gremio
+Phebus novo more
+risum dat, hoc vario
+iam stipate flore.`,
+				``]
+			,
+			[`Zephyrus nectareo
+spirans in odore.
+Certatim pro bravio
+curramus in amore.`,``]
+			,
+[
+`Cytharizat cantico
+dulcis Philomena,
+flore rident vario
+prata iam serena,`
+			,``],
+	[`salit cetus avium
+silve per amena,
+chorus promit virginum
+iam gaudia millena.`,
+	``],
+
+
+
+
+Reclining in Flora's lap
+Phoebus once again
+smiles, now covered
+with many-colored flowers.
+Zephyr breathes
+nectar-scented breezes.
+Let us rush to compete
+in the race of love.
+			With harp-like tones
+the sweet nightingale sings,
+the meadows now laugh
+covered with many flowers,
+a flock of birds takes flight
+through the pleasant forests,
+a chorus of virgins
+promises a thousand joys.
+		]
+
+	}
+]
+
+
+
